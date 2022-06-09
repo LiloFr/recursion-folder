0df53da484038bedf272d217f2145db94ecd19d1
LiloFr <lilo.frich@gmail.com>
2022-06-09 23:34:11 +0300

    заготовки

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import time
+
+
+def way():
+    pass
+
+
+def dictionary():
+    pass
+
+
+def duplicate():
+    pass
+
+
+def my_print():
+    pass
+
+
+if __name__ == '__main__':
+    pass
